34e20ce7c59f8cfccc662cd778e0fcb78ece8fe6
fayasnoushad <thefayasnoushad@gmail.com>
2024-04-07 11:38:13 +0530

    Add meanings settings

--- bot/modules/dictionary/cb.py
+++ bot/modules/dictionary/cb.py
@@ -1,6 +1,6 @@
 from ...database import db
 from .commands import dictionary_help
-from .settings import display_settings, phonetics_settings
+from .settings import display_settings, phonetics_settings, meanings_settings
 
 
 async def dictionary_cb_data(_, message):
@@ -37,24 +37,11 @@ async def dictionary_cb_data(_, message):
                 return
             print(error)
     
-    elif data == "set_meanings":
-        meanings = await db.get_meanings(message.from_user.id)
-        new_type = not meanings
-        await db.update_meanings(message.from_user.id, new_type)
-        if new_type:
-            alert_text = "Meanings enabled successfully"
-        else:
-            alert_text = "Meanings disabled successfully"
-        await message.answer(text=alert_text, show_alert=True)
-        try:
-            await display_settings(_, message, cb=True)
-        except Exception as error:
-            if 'MESSAGE_NOT_MODIFIED' in str(error):
-                return
-            print(error)
-    
     elif data.startswith("phonetics"):
         await phonetics_cb_data(_, message)
+    
+    elif data.startswith("meanings"):
+        await meanings_cb_data(_, message)
 
 
 async def phonetics_cb_data(_, message):
@@ -113,3 +100,110 @@ async def phonetics_cb_data(_, message):
             if 'MESSAGE_NOT_MODIFIED' in str(error):
                 return
             print(error)
+
+
+async def meanings_cb_data(_, message):
+    if message.data.startswith("dictionary"):
+        data = message.data.split("-", 2)[2]
+    else:
+        return
+    
+    if data == "settings":
+        await meanings_settings(_, message, cb=True)
+    
+    elif data == "set_meanings":
+        meanings = await db.get_meanings(message.from_user.id)
+        new_type = not meanings
+        await db.update_meanings(message.from_user.id, new_type)
+        if new_type:
+            alert_text = "Meanings enabled successfully"
+        else:
+            alert_text = "Meanings disabled successfully"
+        await message.answer(text=alert_text, show_alert=True)
+        try:
+            await display_settings(_, message, cb=True)
+        except Exception as error:
+            if 'MESSAGE_NOT_MODIFIED' in str(error):
+                return
+            print(error)
+    
+    elif data == "set_part_of_speech":
+        part_of_speech = await db.get_part_of_speech(message.from_user.id)
+        new_type = not part_of_speech
+        await db.update_part_of_speech(message.from_user.id, new_type)
+        if new_type:
+            alert_text = "Part of speech enabled successfully"
+        else:
+            alert_text = "Part of speech disabled successfully"
+        await message.answer(text=alert_text, show_alert=True)
+        try:
+            await meanings_settings(_, message, cb=True)
+        except Exception as error:
+            if 'MESSAGE_NOT_MODIFIED' in str(error):
+                return
+            print(error)
+    
+    elif data == "set_definitions":
+        definitions = await db.get_definitions(message.from_user.id)
+        new_type = not definitions
+        await db.update_definitions(message.from_user.id, new_type)
+        if new_type:
+            alert_text = "Definitions enabled successfully"
+        else:
+            alert_text = "Definitions disabled successfully"
+        await message.answer(text=alert_text, show_alert=True)
+        try:
+            await meanings_settings(_, message, cb=True)
+        except Exception as error:
+            if 'MESSAGE_NOT_MODIFIED' in str(error):
+                return
+            print(error)
+    
+    elif data == "set_example":
+        example = await db.get_example(message.from_user.id)
+        new_type = not example
+        await db.update_example(message.from_user.id, new_type)
+        if new_type:
+            alert_text = "Example enabled successfully"
+        else:
+            alert_text = "Example disabled successfully"
+        await message.answer(text=alert_text, show_alert=True)
+        try:
+            await meanings_settings(_, message, cb=True)
+        except Exception as error:
+            if 'MESSAGE_NOT_MODIFIED' in str(error):
+                return
+            print(error)
+    
+    elif data == "set_synonyms":
+        synonyms = await db.get_synonyms(message.from_user.id)
+        new_type = not synonyms
+        await db.update_synonyms(message.from_user.id, new_type)
+        if new_type:
+            alert_text = "Synonyms enabled successfully"
+        else:
+            alert_text = "Synonyms disabled successfully"
+        await message.answer(text=alert_text, show_alert=True)
+        try:
+            await meanings_settings(_, message, cb=True)
+        except Exception as error:
+            if 'MESSAGE_NOT_MODIFIED' in str(error):
+                return
+            print(error)
+    
+    elif data == "set_antonyms":
+        antonyms = await db.get_antonyms(message.from_user.id)
+        new_type = not antonyms
+        await db.update_antonyms(message.from_user.id, new_type)
+        if new_type:
+            alert_text = "Antonyms enabled successfully"
+        else:
+            alert_text = "Antonyms disabled successfully"
+        await message.answer(text=alert_text, show_alert=True)
+        try:
+            await meanings_settings(_, message, cb=True)
+        except Exception as error:
+            if 'MESSAGE_NOT_MODIFIED' in str(error):
+                return
+            print(error)
+    
--- bot/modules/dictionary/settings.py
+++ bot/modules/dictionary/settings.py
@@ -51,15 +51,23 @@ async def display_settings(_, message, cb=False):
     origin = await db.get_origin(user_id)
     text += f"\n**Origin:** `{'Yes' if origin else 'No'}`"
     buttons.append(
-        [InlineKeyboardButton(f"Origin - {'✅' if origin else '❎'}", callback_data="dictionary-set_origin")]
+        [
+            InlineKeyboardButton(
+                f"Origin - {'✅' if origin else '❎'}",
+                callback_data="dictionary-set_origin")
+        ]
     )
     meanings = await db.get_meanings(user_id)
     text += f"\n**Meanings:** `{'Yes' if meanings else 'No'}`"
     buttons.append(
-        [InlineKeyboardButton(f"Meanings - {'✅' if meanings else '❎'}", callback_data="dictionary-set_meanings")]
+        [
+            InlineKeyboardButton(
+                f"Meanings - {'✅' if meanings else '❎'}",
+                callback_data="dictionary-meanings-set_meanings")
+        ]
     )
     buttons.append(
-        [InlineKeyboardButton("Meanings Settings", callback_data="dictionary-meanings_settings")]
+        [InlineKeyboardButton("Meanings Settings", callback_data="dictionary-meanings-settings")]
     )
     help_buttons = [
         InlineKeyboardButton("Dictionary Help", callback_data="dictionary-help"),
@@ -144,3 +152,77 @@ async def phonetics_settings(_, message, cb=False):
             )
     except Exception as error:
         print(error)
+
+
+async def meanings_settings(_, message, cb=False):
+        
+        # authorising
+        if not (await auth(message.from_user.id)):
+            return
+        
+        user_id = message.from_user.id
+        meanings = await db.get_meanings(user_id)
+        part_of_speech = await db.get_part_of_speech(user_id)
+        definitions = await db.get_definitions(user_id)
+        example = await db.get_example(user_id)
+        synonyms = await db.get_synonyms(user_id)
+        antonyms = await db.get_antonyms(user_id)
+        text = f"--**Meanings Settings**--\n"
+        text += f"\n**Meanings:** `{'Enabled' if meanings else 'Disabled'}`"
+        text += f"\n**Part of Speech:** `{'Yes' if part_of_speech else 'No'}`"
+        text += f"\n**Definitions:** `{'Yes' if definitions else 'No'}`"
+        text += f"\n**Example:** `{'Yes' if example else 'No'}`"
+        text += f"\n**Synonyms:** `{'Yes' if synonyms else 'No'}`"
+        text += f"\n**Antonyms:** `{'Yes' if antonyms else 'No'}`"
+        buttons = [
+            [
+                InlineKeyboardButton(
+                    f"Part of Speech - {'✅' if part_of_speech else '❎'}",
+                    callback_data="dictionary-meanings-set_part_of_speech")
+            ],
+            [
+                InlineKeyboardButton(
+                    f"Definitions - {'✅' if definitions else '❎'}",
+                    callback_data="dictionary-meanings-set_definitions"),
+                InlineKeyboardButton(
+                    f"Example - {'✅' if example else '❎'}",
+                    callback_data="dictionary-meanings-set_example"),
+            ],
+            [
+                InlineKeyboardButton(
+                    f"Synonyms - {'✅' if synonyms else '❎'}",
+                    callback_data="dictionary-meanings-set_synonyms"),
+                InlineKeyboardButton(
+                    f"Antonyms - {'✅' if antonyms else '❎'}",
+                    callback_data="dictionary-meanings-set_antonyms")
+            ],
+            [InlineKeyboardButton("Back to Settings", callback_data="dictionary-settings")]
+        ]
+        help_buttons = [
+            InlineKeyboardButton("Dictionary Help", callback_data="dictionary-help"),
+            MORE_HELP
+        ]
+        close_btn = [
+            InlineKeyboardButton('Close ✖️', callback_data='close')
+        ]
+        all_buttons = []
+        all_buttons.extend(buttons)
+        all_buttons.append(help_buttons)
+        all_buttons.append(close_btn)
+        settings_buttons = InlineKeyboardMarkup(all_buttons)
+        try:
+            if cb:
+                await message.message.edit_text(
+                    text=text,
+                    disable_web_page_preview=True,
+                    reply_markup=settings_buttons
+                )
+            else:
+                await message.reply_text(
+                    text=text,
+                    quote=True,
+                    disable_web_page_preview=True,
+                    reply_markup=settings_buttons
+                )
+        except Exception as error:
+            print(error)
